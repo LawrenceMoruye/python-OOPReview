7f53add951b6e5ff4af7f3eea824cd588610fc78
LawrenceMoruye <moruyelawrence@gmail.com>
2019-05-18 14:03:11 -0400

    created a class employee explaying instance variables

---
+++ oop1.py
@@ -0,0 +1,16 @@
+class Employee:
+	def __init__(self,first,last,pay):
+		self.first=first
+		self.last=last
+		self.pay=pay
+		self.email=first+last+'@gmail.com'
+
+	def fullname(self):#all functions in a class take self as the first argument
+		return "{} {}".format(self.first,self.last)
+
+
+emp1=Employee("lawrence","moruye",10000)#creating first instance
+emp2=Employee("leony","kemmy",5000)#creating 2nd instance
+print(emp1.email)#obtain the name of employee1
+print(emp2.fullname())#name of employee 2
+print(Employee.fullname(emp1))#fullname of emp2
